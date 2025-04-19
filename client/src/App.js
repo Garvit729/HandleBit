75c195b8be9f14c040b0f280947c5f8e850f88a7
dusht-developer-96 <sharmayur96@gmail.com>
2025-04-19 14:31:26 +0530

    frontend setup and state

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+const App = () => {
+  return (
+    <div> HandleBit Home Page</div>
+  )
+}
+
+export default App
