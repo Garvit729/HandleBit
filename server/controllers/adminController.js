5547d52c63d947843f26de87a9a072be85249aa5
garvit <garvitjain729@gmail.com>
2025-04-21 22:06:28 +0530

    validation middleware and convenor functions added

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -37,4 +37,109 @@ export const logout = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+//@desc     add a new convenor
+//@route    POST /admin/addConvenor
+//@access   private {admin}
+export const addConvenor = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password, committeeId, committeeName, mobile } =
+      req.body;
+    const lcEmail = email.toLowerCase();
+    const user = await Admin.findOne({ email: lcEmail });
+    if (user) {
+      res.status(409).json({ error: "Email already exits" });
+    } else {
+      const salt = await bcrypt.genSalt();
+      const passwordHash = await bcrypt.hash(password, salt);
+      let existingConvenor = await Admin.findOne({role:"convenor",committeeId});
+      if (existingConvenor) {
+
+        // change convenor
+        existingConvenor.name= name;
+        existingConvenor.email= email;
+        existingConvenor.password= password;
+        existingConvenor.mobile=mobile;
+
+        const filterCommittee = { _id: committeeId };
+        const updateCommittee = {
+          convenorName: existingConvenor.name,
+          convenorId: existingConvenor._id,
+        };
+        const updatedCommittee = await Committee.findOneAndUpdate(
+          filterCommittee,
+          updateCommittee,
+          { new: true }
+        );
+        res.status(201).json({ updatedConvenor:existingConvenor, updatedCommittee });
+      } else {
+        const newConvenor = new Admin({
+          email: lcEmail,
+          password: passwordHash,
+          name,
+          role:"convenor",
+          committeeName,
+          committeeId,
+          mobile,
+        });
+        const savedConvenor = await newConvenor.save();
+        const filter = { _id: committeeId };
+        const update = {
+          convenorName: savedConvenor.name,
+          convenorId: savedConvenor._id,
+        };
+        const updatedCommittee = await Committee.findOneAndUpdate(
+          filter,
+          update,
+          { new: true }
+        );
+        res.status(201).json({ savedConvenor, updatedCommittee });
+      }
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//@desc     get list of convenors
+//@route    GET /admin/convenors
+//@access   private {admin}
+export const getConvenors = async (req, res) => {
+  try {
+    const convenors = await Admin.find({ role: "convenor" }).select(
+      "-password"
+    );
+    res.status(200).json(convenors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//@desc     delete a  convenor
+//@route    POST /admin/deleteConvenor
+//@access   private {admin}
+export const deleteConvenor = async (req, res) => {
+  try {
+    const { convenorId, committeeId } = req.body;
+    const deletedConvenor = await Admin.deleteOne({ _id: convenorId });
+    if (deletedConvenor) {
+      const filter = { _id: committeeId };
+      const update = { convenorName: "-", convenorId: "-" };
+      const updatedCommittee = await Committee.findOneAndUpdate(
+        filter,
+        update,
+        { new: true }
+      );
+      res.status(201).json({ msg: "Deleted Successfully" });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message, msg: error.message });
+  }
+};
 
