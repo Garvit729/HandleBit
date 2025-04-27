9ea9520fc996de0ee304c30e5495256ccfaa1ad9
garvit <garvitjain729@gmail.com>
2025-04-27 10:25:11 +0530

    add delet members convenors added in admin controllers

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,10 +3,20 @@ import {
   getConvenors,
   verifyAdmin,
   addConvenor,
+  getMembers,
+  addMember,
+  getCommitteeMembers,
   deleteConvenor,
+  deleteMember,
+  changePassword,
   logout,
 } from "../controllers/adminController.js";
 import { checkRole } from "../middleware/authMiddleware.js";
+import {
+  changePasswordValidationRules,
+  convenorValidationRules,
+  memberValidationRules,
+} from "../middleware/validationMiddleware.js";
 import {
   changePasswordValidationRules,
   convenorValidationRules,
@@ -27,6 +37,25 @@ router.post(
   addConvenor
 );
 router.post("/deleteConvenor", checkRole(["admin"]), deleteConvenor);
+router.get("/members", checkRole(["admin"]), getMembers);
+router.post(
+  "/addMember",
+  checkRole(["admin", "convenor"]),
+  memberValidationRules,
+  addMember
+);
+router.post("/deleteMember", checkRole(["admin", "convenor"]), deleteMember);
+router.post(
+  "/changePassword",
+  checkRole(["admin", "convenor", "member"]),
+  changePasswordValidationRules,
+  changePassword
+);
+router.post(
+  "/committeeMembers",
+  checkRole(["member", "convenor"]),
+  getCommitteeMembers
+);
 
 
 
